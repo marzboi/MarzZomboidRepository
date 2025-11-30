fd40283adf0f16463a50b7925e7cca93d5a004b9
Marco <marcofratini@outlook.com>
2025-12-01 00:36:11 +0100

    Adjust VFE and VFES patches

--- [41-42] Hot_Brass_Mod_Support_Repo/Contents/mods/Hot_Brass_VFE_Support/media/lua/client/VFE_HotBrass_BulletCasingEffect_Overwrite.lua
+++ [41-42] Hot_Brass_Mod_Support_Repo/Contents/mods/Hot_Brass_VFE_Support/media/lua/client/VFE_HotBrass_BulletCasingEffect_Overwrite.lua
@@ -1,3 +1,5 @@
+require "HBVCEF_BulletCasingEffect"
+
 local orig_SpentCasingPhysics_spawnCasing = SpentCasingPhysics.spawnCasing
 
 local m60LinkParam = {
@@ -11,35 +13,31 @@ local m60LinkParam = {
     heightSpreed   = 60
 }
 
-Events.OnGameBoot.Add(function()
-    if Events.OnWeaponSwing and Events.OnWeaponSwing.Remove and orig_SpentCasingPhysics_spawnCasing then
-        Events.OnWeaponSwing.Remove(orig_SpentCasingPhysics_spawnCasing)
-    end
+Events.OnWeaponSwing.Remove(orig_SpentCasingPhysics_spawnCasing)
 
-    function SpentCasingPhysics.spawnCasing(player, weapon)
-        if not player or player:isDead() then return end
-        if not weapon then return end
+function SpentCasingPhysics.spawnCasing(player, weapon)
+    if not player or player:isDead() then return end
+    if not weapon then return end
 
-        local params = SpentCasingPhysics.WeaponEjectionPortParams[weapon:getFullType()]
-        if not params then return end
+    local params = SpentCasingPhysics.WeaponEjectionPortParams[weapon:getFullType()]
+    if not params then return end
 
-        if params.manualEjection then return end
+    if params.manualEjection then return end
 
-        if weapon:isRoundChambered() and not weapon:isJammed() and weapon:haveChamber() then
-            if weapon:hasTag("M60_Link") then
-                local brassCatcher = weapon:getRecoilpad()
-                if brassCatcher then
-                    player:getInventory():AddItem("Base.308Bullets_Casing")
-                    player:getInventory():AddItem("Base.M60_Link")
-                else
-                    SpentCasingPhysics.doSpawnCasing(player, weapon, params)
-                    SpentCasingPhysics.doSpawnCasing(player, weapon, m60LinkParam)
-                end
+    if weapon:isRoundChambered() and not weapon:isJammed() and weapon:haveChamber() then
+        if weapon:hasTag("M60_Link") then
+            local brassCatcher = weapon:getRecoilpad()
+            if brassCatcher then
+                player:getInventory():AddItem("Base.308Bullets_Casing")
+                player:getInventory():AddItem("Base.M60_Link")
             else
                 SpentCasingPhysics.doSpawnCasing(player, weapon, params)
+                SpentCasingPhysics.doSpawnCasing(player, weapon, m60LinkParam)
             end
+        else
+            SpentCasingPhysics.doSpawnCasing(player, weapon, params)
         end
     end
+end
 
-    Events.OnWeaponSwing.Add(SpentCasingPhysics.spawnCasing)
-end)
+Events.OnWeaponSwing.Add(SpentCasingPhysics.spawnCasing)
