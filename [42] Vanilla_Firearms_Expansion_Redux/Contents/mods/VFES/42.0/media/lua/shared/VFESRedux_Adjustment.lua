ff5183171ab3923f5738f2b967be48af7cf66038
Marco <marcofratini@outlook.com>
2025-12-07 14:43:46 +0100

    Add new model type for 9x39 casings

--- [42] Vanilla_Firearms_Expansion_Redux/Contents/mods/VFES/42.0/media/lua/shared/VFESRedux_Adjustment.lua
+++
@@ -1,8 +0,0 @@
-local function Adjust(Name, Property, Value)
-    local Item = ScriptManager.instance:getItem(Name)
-    if not Item then return end
-    Item:DoParam(Property .. " = " .. Value)
-end
-
--- Hot Brass Bullets --
-Adjust("Base.545Bullets", "WorldStaticModel", "New_545_Round")
