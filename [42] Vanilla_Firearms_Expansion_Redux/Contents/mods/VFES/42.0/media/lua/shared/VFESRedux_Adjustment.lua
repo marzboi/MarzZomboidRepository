79b9b7d821bdc867118bb63308b51eb44c8953e6
Marco <marcofratini@outlook.com>
2025-12-07 16:03:22 +0100

    change names of icons

---
+++ [42] Vanilla_Firearms_Expansion_Redux/Contents/mods/VFES/42.0/media/lua/shared/VFESRedux_Adjustment.lua
@@ -0,0 +1,9 @@
+local function Adjust(Name, Property, Value)
+    local Item = ScriptManager.instance:getItem(Name)
+    if not Item then return end
+    Item:DoParam(Property .. " = " .. Value)
+end
+
+-- Hot Brass Bullets --
+Adjust("Base.545Bullets", "icon", "545_Round")
+Adjust("Base.545Bullets", "WorldStaticModel", "New_545_Round")
