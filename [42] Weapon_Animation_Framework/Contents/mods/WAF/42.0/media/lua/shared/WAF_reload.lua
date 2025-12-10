1146da4f9c26bb761ae5b11cc26e909015f6292f
Marco <marcofratini@outlook.com>
2025-12-10 20:01:53 +0100

    Sync cycling to racking

--- [42] Weapon_Animation_Framework/Contents/mods/WAF/42.0/media/lua/shared/WAF_reload.lua
+++ [42] Weapon_Animation_Framework/Contents/mods/WAF/42.0/media/lua/shared/WAF_reload.lua
@@ -1,8 +1,20 @@
 require "TimedActions/ISRackFirearm"
 
-local rackBullet = ISRackFirearm.rackBullet
+-- local rackBullet = ISRackFirearm.rackBullet
 
-function ISRackFirearm:rackBullet()
-    rackBullet(self)
-    AnimationWeaponAction.animateWeaponFiring(self.character, self.gun, 10)
+-- function ISRackFirearm:rackBullet()
+--     rackBullet(self)
+--     AnimationWeaponAction.animateWeaponFiring(self.character, self.gun, 10)
+-- end
+
+local ISRackFirearm_animEvent_old = ISRackFirearm.animEvent
+function ISRackFirearm:animEvent(event, parameter)
+    if event == 'playReloadSound' then
+        if parameter == 'rack' then
+            if self.gun:getRackSound() then
+                AnimationWeaponAction.animateWeaponFiring(self.character, self.gun, 10)
+            end
+        end
+    end
+    return ISRackFirearm_animEvent_old(self, event, parameter)
 end
