dc4e1e21c92f82d95b8d5d9710dabfda39196e47
Marco <marcofratini@outlook.com>
2025-12-13 21:41:49 +0100

    Add script to add new types into mapper

--- [42] Vanilla_Firearms_Expansion_Redux/Contents/mods/VFE/42.0/media/lua/server/VFERedux_Ammo_Recipes.lua
+++ [42] Vanilla_Firearms_Expansion_Redux/Contents/mods/VFE/42.0/media/lua/server/VFERedux_Ammo_Recipes.lua
@@ -1,34 +1,102 @@
 --- Gonna remove vanilla cartons and use my condense carton recipe
-Events.OnGameStart.Add(function()
-    local recipe = getScriptManager():getCraftRecipe("PlaceBoxesOf556AmmoInCarton")
-    if not recipe then return end
-    recipe:getInputs():clear()
+local recipesToRemove = {
+    "PlaceBoxesOf556AmmoInCarton",
+    "PlaceBoxesOf223AmmoInCarton",
+    "PlaceBoxesOf308AmmoInCarton",
+    "PlaceBoxesOf9mmBulletsInCarton",
+    "PlaceBoxesOf45AutoBulletsInCarton",
+    "PlaceBoxesOf38SpecialBulletsInCarton",
+    "PlaceBoxesOf44MagnumBulletsInCarton",
+    "PlaceBoxesOfShotgunShellsInCarton"
+}
 
-    local recipe = getScriptManager():getCraftRecipe("PlaceBoxesOf223AmmoInCarton")
-    if not recipe then return end
-    recipe:getInputs():clear()
+-- Remove Individual Carton CraftRecipes
+for i = 1, #recipesToRemove do
+    local recipe = getScriptManager():getCraftRecipe(recipesToRemove[i])
+    if recipe then
+        recipe:getInputs():clear()
+        recipe:Load(recipe:getName(), [[
+        {
+            inputs {
+                item 1 [Base.FakeItem],
+        }
+        ]])
+    end
+end
 
-    local recipe = getScriptManager():getCraftRecipe("PlaceBoxesOf308AmmoInCarton")
-    if not recipe then return end
-    recipe:getInputs():clear()
+-- Add new ammotypes to open box of 50 bullets
+local recipe = getScriptManager():getCraftRecipe("OpenBoxOfBullets50")
+if not recipe then return end
+recipe:getInputs():clear()
+recipe:Load(recipe:getName(), [[
+    {
+    inputs
+        {
+            item 1 [Base.Bullets9mmBox;Base.Bullets45Box;Base.Bullets38Box;Base.223Box;Base.556Box;Base.762Box;Base.22Box]
+            mappers[ammoTypes]
+            flags[Prop2;AllowFavorite;InheritFavorite],
+        }
+    }
+    ]]
+)
+recipe:Load(recipe:getName(), [[
+    {
+    itemMapper ammoTypes
+        {
+            Base.762Bullets = Base.762Box,
+            Base.22Bullets  = Base.22Box,
+        }
+    }
+    ]])
 
-    local recipe = getScriptManager():getCraftRecipe("PlaceBoxesOf9mmBulletsInCarton")
-    if not recipe then return end
-    recipe:getInputs():clear()
+-- Add new ammotypes to open cartons of boxes scripts
+local recipe = getScriptManager():getCraftRecipe("OpenCartonOfBullets")
+if not recipe then return end
+recipe:getInputs():clear()
+recipe:Load(recipe:getName(), [[
+    {
+    inputs
+        {
+            item 1 [Base.Bullets9mmCarton;Base.Bullets45Carton;Base.Bullets38Carton;Base.Bullets44Carton;Base.223Carton;Base.308Carton;Base.ShotgunShellsCarton;Base.556Carton;Base.762Carton;Base.22Carton]
+            mappers[ammoTypes]
+            flags[AllowFavorite;InheritFavorite],
+        }
+    }
+    ]]
+)
+recipe:Load(recipe:getName(), [[
+    {
+        itemMapper ammoTypes
+        {
+            Base.762Box           = Base.762Carton,
+            Base.22Box            = Base.22Carton,
+        }
+    }
+    ]]
+)
 
-    local recipe = getScriptManager():getCraftRecipe("PlaceBoxesOf45AutoBulletsInCarton")
-    if not recipe then return end
-    recipe:getInputs():clear()
-
-    local recipe = getScriptManager():getCraftRecipe("PlaceBoxesOf38SpecialBulletsInCarton")
-    if not recipe then return end
-    recipe:getInputs():clear()
-
-    local recipe = getScriptManager():getCraftRecipe("PlaceBoxesOf44MagnumBulletsInCarton")
-    if not recipe then return end
-    recipe:getInputs():clear()
-
-    local recipe = getScriptManager():getCraftRecipe("PlaceBoxesOfShotgunShellsInCarton")
-    if not recipe then return end
-    recipe:getInputs():clear()
-end)
+-- Add new ammotypes to put bullets in boxes scripts
+local recipe = getScriptManager():getCraftRecipe("place_ammo_in_box")
+if not recipe then return end
+recipe:getInputs():clear()
+recipe:Load(recipe:getName(), [[
+    {
+    inputs
+        {
+            item 20 [Base.Bullets44;Base.308Bullets;25:Base.ShotgunShells;50:Base.223Bullets;50:Base.556Bullets;50:Base.Bullets9mm;50:Base.Bullets45;50:Base.Bullets38;50:Base.762Bullets;50:Base.22Bullets]
+            mappers[ammoType]
+            flags[AllowFavorite;InheritFavorite;IsExclusive],
+        }
+    }
+    ]]
+)
+recipe:Load(recipe:getName(), [[
+    {
+        itemMapper ammoType
+        {
+            Base.762Box           = Base.762Bullets,
+            Base.22Box            = Base.22Bullets,
+        }
+    }
+    ]]
+)
