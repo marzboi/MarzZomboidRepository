a2aee01e549bc214ebb58b7f30b683934651ed8b
Marco <marcofratini@outlook.com>
2025-12-16 15:26:37 +0100

    Restore old version of WAF

--- [42] Weapon_Animation_Framework/Contents/mods/WAF/42.0/media/lua/client/WAF_Animations.lua
+++ [42] Weapon_Animation_Framework/Contents/mods/WAF/42.0/media/lua/client/WAF_Animations.lua
@@ -67,27 +67,49 @@ function AnimationWeaponAction.onTick()
     end
 end
 
-function AnimationWeaponAction.rackAction(player, weapon, percentage)
-    if not (player and weapon) then return end
+-- function AnimationWeaponAction.rackAction(player, weapon, percentage)
+--     if not (player and weapon) then return end
 
-    local slide = weapon:getWeaponPart("Slide")
-    if not slide then return end
+--     local slide = weapon:getWeaponPart("Slide")
+--     if not slide then return end
 
-    local pctToStep = { [0] = 0, [10] = 1, [20] = 2, [30] = 3, [40] = 4, [50] = 5, [60] = 4, [70] = 3, [80] = 2, [90] = 1,
-        [100] = 0 }
-    local step = pctToStep[percentage]
+--     local pctToStep = {
+--         [0] = 0,
+--         [10] = 1,
+--         [20] = 2,
+--         [30] = 3,
+--         [40] = 4,
+--         [50] = 5,
+--         [60] = 4,
+--         [70] = 3,
+--         [80] = 2,
+--         [90] = 1,
+--         [100] = 0
+--     }
+--     local step = pctToStep[percentage]
 
-    local slideType = slide:getFullType()
-    local targetType = "SlideAttachment"
+--     local slideType = slide:getFullType()
+--     local targetType = "SlideAttachment"
 
-    if step ~= nil then
-        targetType = ("SlideAttachment%02d"):format(step * 10)
-        if slideType == "SlideAttachment50" and step < 5 and percentage <= 50 then return end
-        if slideType == targetType then return end
+--     if step ~= nil then
+--         targetType = ("SlideAttachment%02d"):format(step * 10)
+--         if slideType == "SlideAttachment50" and step < 5 and percentage <= 50 then return end
+--         if slideType == targetType then return end
+--     end
+
+--     weapon:attachWeaponPart(instanceItem(targetType), true)
+--     player:resetEquippedHandsModels()
+-- end
+
+function AnimationWeaponAction.rackAction(player, weapon, starting)
+    if not weapon or not player then return end
+    if starting then
+        weapon:attachWeaponPart(instanceItem("SlideAttachment50"), true)
+        player:resetEquippedHandsModels()
+    else
+        weapon:attachWeaponPart(instanceItem("SlideAttachment"), true)
+        player:resetEquippedHandsModels()
     end
-
-    weapon:attachWeaponPart(instanceItem(targetType), true)
-    player:resetEquippedHandsModels()
 end
 
 Events.OnEquipPrimary.Add(AnimationWeaponAction.attachPart)
