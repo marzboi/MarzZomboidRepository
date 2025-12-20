ed0d3afd822878f0ad8e8816dec405de03df99a6
Marco <marcofratini@outlook.com>
2025-12-20 13:29:22 +0100

    Add new round families, move logic to do params at last

--- [41-42] Hot_Brass_Visible_Casing_Ejection_Framework/Contents/mods/Hot_Brass_Visible_Casing_Ejection_Framework/42.13/media/lua/shared/HBVCEF_ItemAdjustment.lua
+++ [41-42] Hot_Brass_Visible_Casing_Ejection_Framework/Contents/mods/Hot_Brass_Visible_Casing_Ejection_Framework/42.13/media/lua/shared/HBVCEF_ItemAdjustment.lua
@@ -4,26 +4,56 @@ local function Adjust(Name, Property, Value)
     Item:DoParam(Property .. " = " .. Value)
 end
 
--- AMMO MODELS
-Adjust("Base.Bullets9mm", "WorldStaticModel", "New_9mm_Round")
-Adjust("Base.Bullets45", "WorldStaticModel", "New_45_Round")
-Adjust("Base.Bullets44", "WorldStaticModel", "New_44_Round")
-Adjust("Base.Bullets38", "WorldStaticModel", "New_38_Round")
-Adjust("Base.308Bullets", "WorldStaticModel", "New_308_Round")
-Adjust("Base.223Bullets", "WorldStaticModel", "New_223_Round")
-Adjust("Base.556Bullets", "WorldStaticModel", "New_556_Round")
-Adjust("Base.ShotgunShells", "WorldStaticModel", "New_Shotgun_Round")
+Events.OnInitWorld.Add(function()
+    -- --Shotgun Modifiers
+    Adjust("Base.DoubleBarrelShotgun", "ManuallyRemoveSpentRounds", "true")
+    Adjust("Base.DoubleBarrelShotgunSawnoff", "ManuallyRemoveSpentRounds", "true")
 
--- AMMO ICONS
-Adjust("Base.ShotgunShells", "icon", "Shotgun_Round")
-Adjust("Base.Bullets9mm", "icon", "9mm_Round")
-Adjust("Base.Bullets45", "icon", "45_Round")
-Adjust("Base.Bullets44", "icon", "44_Round")
-Adjust("Base.Bullets38", "icon", "38_Round")
-Adjust("Base.308Bullets", "icon", "308_Round")
-Adjust("Base.556Bullets", "icon", "556_Round")
-Adjust("Base.223Bullets", "icon", "223_Round")
+    -- Vanilla
+    -- WorldStaticModel
+    Adjust("Base.223Bullets", "WorldStaticModel", "New_223_Round")
+    Adjust("Base.308Bullets", "WorldStaticModel", "New_308_Round")
+    Adjust("Base.556Bullets", "WorldStaticModel", "New_556_Round")
+    Adjust("Base.Bullets38", "WorldStaticModel", "New_38_Round")
+    Adjust("Base.Bullets44", "WorldStaticModel", "New_44_Round")
+    Adjust("Base.Bullets45", "WorldStaticModel", "New_45_Round")
+    Adjust("Base.Bullets9mm", "WorldStaticModel", "New_9mm_Round")
+    Adjust("Base.ShotgunShells", "WorldStaticModel", "New_Shotgun_Round")
+    -- Ammo icons
+    Adjust("Base.223Bullets", "icon", "223_Round")
+    Adjust("Base.308Bullets", "icon", "308_Round")
+    Adjust("Base.556Bullets", "icon", "556_Round")
+    Adjust("Base.Bullets38", "icon", "38_Round")
+    Adjust("Base.Bullets44", "icon", "44_Round")
+    Adjust("Base.Bullets45", "icon", "45_Round")
+    Adjust("Base.Bullets9mm", "icon", "9mm_Round")
+    Adjust("Base.ShotgunShells", "icon", "Shotgun_Round")
 
--- --Shotgun Modifiers
-Adjust("Base.DoubleBarrelShotgun", "ManuallyRemoveSpentRounds", "true")
-Adjust("Base.DoubleBarrelShotgunSawnoff", "ManuallyRemoveSpentRounds", "true")
+    -- VFE and Addons
+    -- WorldStaticModel
+    Adjust("Base.22Bullets", "WorldStaticModel", "New_22_Round")
+    Adjust("Base.308BulletsLinked", "WorldStaticModel", "New_Linked308_Round")
+    Adjust("Base.46Bullets", "WorldStaticModel", "New_46_Round")
+    Adjust("Base.545Bullets", "WorldStaticModel", "New_545_Round")
+    Adjust("Base.57Bullets", "WorldStaticModel", "New_57_Round")
+    Adjust("Base.76254Bullets", "WorldStaticModel", "New_76254_Round")
+    Adjust("Base.762Bullets", "WorldStaticModel", "New_762_Round")
+    Adjust("Base.939Bullets", "WorldStaticModel", "New_939_Round")
+    -- Ammo icons
+    Adjust("Base.22Bullets", "icon", "22_Round")
+    Adjust("Base.308BulletsLinked", "icon", "Linked308_Round")
+    Adjust("Base.46Bullets", "icon", "46_Round")
+    Adjust("Base.545Bullets", "icon", "545_Round")
+    Adjust("Base.57Bullets", "icon", "57_Round")
+    Adjust("Base.76254Bullets", "icon", "76254_Round")
+    Adjust("Base.762Bullets", "icon", "762_Round")
+    Adjust("Base.939Bullets", "icon", "939_Round")
+
+    -- Guns of 93
+    -- WorldStaticModel
+    Adjust("Base.3006Bullets", "WorldStaticModel", "New_3006_Round")
+    Adjust("Base.792Bullets", "WorldStaticModel", "New_792_Round")
+    -- Ammo icons
+    Adjust("Base.3006Bullets", "icon", "3006_Round")
+    Adjust("Base.792Bullets", "icon", "792_Round")
+end)
