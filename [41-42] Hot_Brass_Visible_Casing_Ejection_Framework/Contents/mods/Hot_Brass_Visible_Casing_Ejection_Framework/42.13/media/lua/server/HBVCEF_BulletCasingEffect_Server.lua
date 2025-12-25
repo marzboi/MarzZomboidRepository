0c7d74ada949a5254d39edae7af36f8bc32b1b8b
Marco <marcofratini@outlook.com>
2025-12-25 21:58:26 +0100

    merga functions together

--- [41-42] Hot_Brass_Visible_Casing_Ejection_Framework/Contents/mods/Hot_Brass_Visible_Casing_Ejection_Framework/42.13/media/lua/server/HBVCEF_BulletCasingEffect_Server.lua
+++ [41-42] Hot_Brass_Visible_Casing_Ejection_Framework/Contents/mods/Hot_Brass_Visible_Casing_Ejection_Framework/42.13/media/lua/server/HBVCEF_BulletCasingEffect_Server.lua
@@ -228,6 +228,13 @@ end
 function SpentCasingPhysics.playCasingImpactSound(casing, square)
     if not casing then return end
 
+    if SpentCasingPhysics.VFE then
+        if casing.casingType == "Base.M1Bloc" then
+            casing.player:getEmitter():playSound("M1PingDrop")
+            casing.repeatCasingSound = false
+        end
+    end
+
     local family = SpentCasingPhysics.getCasingSoundFamily(casing)
     local surfaceTypeName = SpentCasingPhysics.getSurfaceTypeFromSquare(square)
 
@@ -287,6 +294,7 @@ function SpentCasingPhysics.addCasing(
         currentWorldItem = nil,
         floorBounces = SpentCasingPhysics.RANDOM:random(1, 2),
         hasHitFloor = false,
+        repeatCasingSound = true,
     }
 
     casingData.currentWorldItem = square:AddWorldInventoryItem(casingType, startX, startY, startZ)
@@ -640,7 +648,18 @@ function SpentCasingPhysics.spawnCasing(player, weapon)
         SpentCasingPhysics.DefaultEjectionPortParams[weapon:getWeaponReloadType()]
 
     if weapon:isRoundChambered() and not weapon:isJammed() and weapon:haveChamber() then
-        SpentCasingPhysics.doSpawnCasing(player, weapon, params)
+        if SpentCasingPhysics.VFE and weapon:hasTag(VFETags.m60_link) then
+            local brassCatcher = weapon:getWeaponPart('RecoilPad')
+            if brassCatcher then
+                player:getInventory():AddItem("Base.308Bullets_Casing")
+                player:getInventory():AddItem("Base.M60_Link")
+            else
+                SpentCasingPhysics.doSpawnCasing(player, weapon, params, false, "Base.308Bullets_Casing")
+                SpentCasingPhysics.doSpawnCasing(player, weapon, params, false, "Base.M60_Link")
+            end
+        else
+            SpentCasingPhysics.doSpawnCasing(player, weapon, params)
+        end
     end
 end
 
@@ -652,7 +671,18 @@ function SpentCasingPhysics.rackCasing(player, weapon, racking)
         SpentCasingPhysics.DefaultEjectionPortParams[weapon:getWeaponReloadType()]
 
     if racking then
-        SpentCasingPhysics.doSpawnCasing(player, weapon, params, racking)
+        if SpentCasingPhysics.VFE and weapon:hasTag(VFETags.m60_link) then
+            local brassCatcher = weapon:getWeaponPart('RecoilPad')
+            if brassCatcher then
+                player:getInventory():AddItem("Base.308Bullets")
+                player:getInventory():AddItem("Base.M60_Link")
+            else
+                SpentCasingPhysics.doSpawnCasing(player, weapon, params, racking, "Base.308Bullets")
+                SpentCasingPhysics.doSpawnCasing(player, weapon, params, racking, "Base.M60_Link")
+            end
+        else
+            SpentCasingPhysics.doSpawnCasing(player, weapon, params, racking)
+        end
     end
 
     if not racking then
@@ -689,130 +719,6 @@ end
 
 Events.OnClientCommand.Add(SpentCasingPhysics.onClientCommand)
 Events.OnTick.Add(SpentCasingPhysics.update)
-
-if SpentCasingPhysics.VFE then
-    function SpentCasingPhysics.addCasing(
-        player,
-        weapon,
-        square,
-        casingType,
-        startX,
-        startY,
-        startZ,
-        velocityX,
-        velocityY,
-        velocityZ)
-        if not square then return end
-
-        local casingData = {
-            player = player,
-            weapon = weapon,
-            square = square,
-            casingType = casingType,
-            x = startX,
-            y = startY,
-            z = startZ,
-            velocityX = velocityX or 0,
-            velocityY = velocityY or 0,
-            velocityZ = velocityZ or 0.1,
-            active = true,
-            currentWorldItem = nil,
-            floorBounces = SpentCasingPhysics.RANDOM:random(1, 2),
-            hasHitFloor = false,
-            repeatCasingSound = true,
-        }
-
-        casingData.currentWorldItem = square:AddWorldInventoryItem(casingType, startX, startY, startZ)
-
-        table.insert(SpentCasingPhysics.activeCasings, casingData)
-    end
-
-    function SpentCasingPhysics.playCasingImpactSound(casing, square)
-        if not casing then return end
-
-        if casing.casingType == "Base.M1Bloc" then
-            casing.player:getEmitter():playSound("M1PingDrop")
-            casing.repeatCasingSound = false
-        end
-
-        local family = SpentCasingPhysics.getCasingSoundFamily(casing)
-        local surfaceTypeName = SpentCasingPhysics.getSurfaceTypeFromSquare(square)
-
-        local familyParams = SpentCasingPhysics.CasingImpactSoundParams[family]
-        if not familyParams then return end
-
-        local params = familyParams[surfaceTypeName] or familyParams.Concrete
-        if not params then return end
-
-        local count = params.variations or 1
-        local idx = (count > 1) and SpentCasingPhysics.RANDOM:random(1, count) or 1
-        local soundName = params.prefix .. tostring(idx)
-
-        if isServer() then
-            if casing.player then
-                sendServerCommand(casing.player, "HBVCEF", "playCasingImpactSound", { sound = soundName })
-            else
-                sendServerCommand("HBVCEF", "playCasingImpactSound", { sound = soundName })
-            end
-            return
-        end
-
-        if casing.player then
-            casing.player:getEmitter():playSound(soundName)
-        end
-    end
-
-    function SpentCasingPhysics.spawnCasing(player, weapon)
-        if not player or player:isDead() then return end
-        if not weapon then return end
-        if weapon:isRackAfterShoot() or weapon:isManuallyRemoveSpentRounds() then return end
-
-        local params = SpentCasingPhysics.WeaponEjectionPortParams[weapon:getFullType()] or
-            SpentCasingPhysics.DefaultEjectionPortParams[weapon:getWeaponReloadType()]
-
-        if weapon:isRoundChambered() and not weapon:isJammed() and weapon:haveChamber() then
-            if weapon:hasTag(VFETags.m60_link) then
-                local brassCatcher = weapon:getWeaponPart('RecoilPad')
-                if brassCatcher then
-                    player:getInventory():AddItem("Base.308Bullets_Casing")
-                    player:getInventory():AddItem("Base.M60_Link")
-                else
-                    SpentCasingPhysics.doSpawnCasing(player, weapon, params, false, "Base.308Bullets_Casing")
-                    SpentCasingPhysics.doSpawnCasing(player, weapon, params, false, "Base.M60_Link")
-                end
-            else
-                SpentCasingPhysics.doSpawnCasing(player, weapon, params)
-            end
-        end
-    end
-
-    function SpentCasingPhysics.rackCasing(player, weapon, racking)
-        if not player or player:isDead() then return end
-        if not weapon then return end
-
-        local params = SpentCasingPhysics.WeaponEjectionPortParams[weapon:getFullType()] or
-            SpentCasingPhysics.DefaultEjectionPortParams[weapon:getWeaponReloadType()]
-
-        if racking then
-            if weapon:hasTag(VFETags.m60_link) then
-                local brassCatcher = weapon:getWeaponPart('RecoilPad')
-                if brassCatcher then
-                    player:getInventory():AddItem("Base.308Bullets")
-                    player:getInventory():AddItem("Base.M60_Link")
-                else
-                    SpentCasingPhysics.doSpawnCasing(player, weapon, params, racking, "Base.308Bullets")
-                    SpentCasingPhysics.doSpawnCasing(player, weapon, params, racking, "Base.M60_Link")
-                end
-            else
-                SpentCasingPhysics.doSpawnCasing(player, weapon, params, racking)
-            end
-        end
-
-        if not racking then
-            SpentCasingPhysics.doSpawnCasing(player, weapon, params)
-        end
-    end
-end
 
 if SpentCasingPhysics.GGS then
     Events.OnWeaponSwing.Remove(handleWeaponShot)
