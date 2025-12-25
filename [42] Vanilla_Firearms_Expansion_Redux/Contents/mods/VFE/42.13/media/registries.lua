f93dc714e6cf64bb1d5696a52f2febc02c8e6362
Marco <marcofratini@outlook.com>
2025-12-25 20:41:50 +0100

    Remove comments in registries

--- [42] Vanilla_Firearms_Expansion_Redux/Contents/mods/VFE/42.13/media/registries.lua
+++ [42] Vanilla_Firearms_Expansion_Redux/Contents/mods/VFE/42.13/media/registries.lua
@@ -9,23 +9,3 @@ VFETags.catchbrass = ItemTag.register("vfe:catchbrass")
 AmmoType.register("vfe:bullets_762", "Base.762Bullets")
 AmmoType.register("vfe:bullets_22", "Base.22Bullets")
 AmmoType.register("vfe:bullets_308_linked", "Base.308BulletsLinked")
-
--- base:shotgun_shells
--- base:bullets_556
--- base:bullets_308
--- base:bullets_9mm
--- base:bullets_45
--- base:bullets_44
--- base:bullets_38
--- base:bullets_223
-
--- vfe:bullets_762
--- vfe:bullets_22
--- vfe:bullets_308_linked
-
--- vfes:bullets_939
--- vfes:bullets_545
--- vfes:bullets_76254
-
--- vfe93:bullets_57
--- vfe93:bullets_46
--- [42] Vanilla_Firearms_Expansion_Redux/Contents/mods/VFES/42.13/media/registries.lua
+++ [42] Vanilla_Firearms_Expansion_Redux/Contents/mods/VFES/42.13/media/registries.lua
@@ -1,23 +1,3 @@
 AmmoType.register("vfes:bullets_939", "Base.939Bullets")
 AmmoType.register("vfes:bullets_545", "Base.545Bullets")
 AmmoType.register("vfes:bullets_76254", "Base.76254Bullets")
-
--- base:shotgun_shells
--- base:bullets_556
--- base:bullets_308
--- base:bullets_9mm
--- base:bullets_45
--- base:bullets_44
--- base:bullets_38
--- base:bullets_223
-
--- vfe:bullets_762
--- vfe:bullets_22
--- vfe:bullets_308_linked
-
--- vfes:bullets_939
--- vfes:bullets_545
--- vfes:bullets_76254
-
--- vfe93:bullets_57
--- vfe93:bullets_46
