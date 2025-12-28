97419b7cac6a8501837db706eff82095d131f042
Marco <marcofratini@outlook.com>
2025-12-28 17:24:52 +0100

    Add modules for VFE

--- [41-42] Hot_Brass_Visible_Casing_Ejection_Framework/Contents/mods/Hot_Brass_Visible_Casing_Ejection_Framework/42.13/media/registries.lua
+++ [41-42] Hot_Brass_Visible_Casing_Ejection_Framework/Contents/mods/Hot_Brass_Visible_Casing_Ejection_Framework/42.13/media/registries.lua
@@ -3,3 +3,4 @@ HBTags = {}
 HBTags.LightCasing = ItemTag.register("hb:lightcasing")
 HBTags.MediumCasing = ItemTag.register("hb:mediumcasing")
 HBTags.HeavyCasing = ItemTag.register("hb:heavycasing")
+HBTags.ShotgunShell = ItemTag.register("hb:shotgunshell")
