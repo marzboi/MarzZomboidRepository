8f436abb5387219a0f2b0ac67f55d13bc3d5d3f3
Marco <marcofratini@outlook.com>
2025-12-29 16:30:05 +0100

    Fix params for the MP5

--- [41-42] Hot_Brass_Visible_Casing_Ejection_Framework/Contents/mods/Hot_Brass_Visible_Casing_Ejection_Framework/42.13/media/lua/server/HBVCEF_Custom_Params_tables.lua
+++ [41-42] Hot_Brass_Visible_Casing_Ejection_Framework/Contents/mods/Hot_Brass_Visible_Casing_Ejection_Framework/42.13/media/lua/server/HBVCEF_Custom_Params_tables.lua
@@ -406,36 +406,36 @@ if SpentCasingPhysics.VFE then
         --- Smgs
 
         ["Base.MP5"] = {
-            forwardOffset = 0.30,
-            sideOffset    = 0.0,
-            heightOffset  = 0.50,
+            forwardOffset = 0.32,
+            sideOffset    = 0.10,
+            heightOffset  = 0.45,
             shellForce    = 0.60,
             sideSpread    = 60,
             heightSpread  = 60,
         },
 
         ["Base.MP5SD"] = {
-            forwardOffset = 0.30,
-            sideOffset    = 0.0,
-            heightOffset  = 0.50,
+            forwardOffset = 0.32,
+            sideOffset    = 0.10,
+            heightOffset  = 0.45,
             shellForce    = 0.60,
             sideSpread    = 60,
             heightSpread  = 60,
         },
 
         ["Base.MP5Unfolded"] = {
-            forwardOffset = 0.30,
-            sideOffset    = 0.0,
-            heightOffset  = 0.50,
+            forwardOffset = 0.32,
+            sideOffset    = 0.10,
+            heightOffset  = 0.45,
             shellForce    = 0.60,
             sideSpread    = 60,
             heightSpread  = 60,
         },
 
         ["Base.MP5Folded"] = {
-            forwardOffset = 0.30,
-            sideOffset    = 0.0,
-            heightOffset  = 0.50,
+            forwardOffset = 0.32,
+            sideOffset    = 0.10,
+            heightOffset  = 0.45,
             shellForce    = 0.60,
             sideSpread    = 60,
             heightSpread  = 60,
