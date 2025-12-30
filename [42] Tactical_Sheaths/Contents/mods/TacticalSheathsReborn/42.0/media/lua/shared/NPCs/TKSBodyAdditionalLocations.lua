2d481f8742331ba36ddffc0813d65261fd92da5d
Marco <marcofratini@outlook.com>
2025-12-30 12:09:12 +0100

    Add body lcoations

--- [42] Tactical_Sheaths/Contents/mods/TacticalSheathsReborn/42.0/media/lua/shared/NPCs/TKSBodyAdditionalLocations.lua
+++ [42] Tactical_Sheaths/Contents/mods/TacticalSheathsReborn/42.0/media/lua/shared/NPCs/TKSBodyAdditionalLocations.lua
@@ -1,18 +1,8 @@
-local AdditionalBodyLocations = {
-    "KnifeSheathLeg",
-    "KnifeSheathBack",
-    "KnifeSheathLeftLeg",
-    "KatanaSheath",
-    "KatanaSheathHip",
-}
+require "BodyLocations"
 
 local group = BodyLocations.getGroup("Human")
-for _, location in ipairs(AdditionalBodyLocations) do
-    local bodyLocation = BodyLocation.new(group, location)
-    group:getAllLocations():add(bodyLocation)
-end
-
-if getActivatedMods():contains("\\PLLootRelooted") then
-    group:setExclusive("KnifeSheathLeg", "PLL Holster Right")
-    group:setExclusive("KnifeSheathLeftLeg", "PLL Holster Left")
-end
+group:getOrCreateLocation(TKSRegistries.BodyLocations.KnifeSheathLeg)
+group:getOrCreateLocation(TKSRegistries.BodyLocations.KnifeSheathBack)
+group:getOrCreateLocation(TKSRegistries.BodyLocations.KnifeSheathLeftLeg)
+group:getOrCreateLocation(TKSRegistries.BodyLocations.KatanaSheath)
+group:getOrCreateLocation(TKSRegistries.BodyLocations.KatanaSheathHip)
