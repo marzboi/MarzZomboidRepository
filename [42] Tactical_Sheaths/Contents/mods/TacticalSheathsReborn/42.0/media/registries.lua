0ba34330572010b8736ed4955860ba6c18b7e47a
Marco <marcofratini@outlook.com>
2025-12-30 12:07:32 +0100

    starting porting tks to 42

---
+++ [42] Tactical_Sheaths/Contents/mods/TacticalSheathsReborn/42.0/media/registries.lua
@@ -0,0 +1,8 @@
+TKSRegistries = {}
+TKSRegistries.BodyLocations = {}
+
+TKSRegistries.BodyLocations.KnifeSheathLeg = ItemBodyLocation.register("tks:KnifeSheathLeg")
+TKSRegistries.BodyLocations.KnifeSheathBack = ItemBodyLocation.register("tks:KnifeSheathBack")
+TKSRegistries.BodyLocations.KnifeSheathLeftLeg = ItemBodyLocation.register("KnifeSheathLeftLeg")
+TKSRegistries.BodyLocations.KatanaSheath = ItemBodyLocation.register("tks:KatanaSheath")
+TKSRegistries.BodyLocations.KatanaSheathHip = ItemBodyLocation.register("tks:KatanaSheathHip")
