dad30636327cecc13c408a223fd44067dcb80d1a
Marco Fratini <marcofratini@outlook.com>
2026-01-06 01:27:05 +0100

    Add hijack version for Guns of 93

--- [42] RPM_Real_Automatic_Firerate/Contents/mods/RAF_Real_Automatic_Firerate/42.13/media/lua/client/RAF_ClientSide.lua
+++ [42] RPM_Real_Automatic_Firerate/Contents/mods/RAF_Real_Automatic_Firerate/42.13/media/lua/client/RAF_ClientSide.lua
@@ -36,29 +36,74 @@ function RAFFunctions.RAF_OnServerCommand(module, command, args)
     end
 end
 
-ISInventoryPaneContextMenu.onChangefiremode = function(playerObj, weapon, newfiremode)
-    if newfiremode == "Auto" then newfiremode = "RealAuto" end
-    if newfiremode == "Burst" then newfiremode = "RealBurst" end
-    weapon:setFireMode(newfiremode)
-    playerObj:setFireMode(newfiremode)
-    RAFFunctions.OnPlayerUpdateFiremode(playerObj, weapon, newfiremode)
-end
+if not RAFEnabledMods.G93 then
+    ISInventoryPaneContextMenu.onChangefiremode = function(playerObj, weapon, newfiremode)
+        if newfiremode == "Auto" then newfiremode = "RealAuto" end
+        if newfiremode == "Burst" then newfiremode = "RealBurst" end
+        weapon:setFireMode(newfiremode)
+        playerObj:setFireMode(newfiremode)
+        RAFFunctions.OnPlayerUpdateFiremode(playerObj, weapon, newfiremode)
+    end
 
-ISInventoryPaneContextMenu.doChangeFireModeMenu = function(playerObj, weapon, context)
-    local firemodeOption = context:addOption(getText("ContextMenu_ChangeFireMode"))
-    local subMenuFiremode = context:getNew(context)
-    context:addSubMenu(firemodeOption, subMenuFiremode)
+    ISInventoryPaneContextMenu.doChangeFireModeMenu = function(playerObj, weapon, context)
+        local firemodeOption = context:addOption(getText("ContextMenu_ChangeFireMode"))
+        local subMenuFiremode = context:getNew(context)
+        context:addSubMenu(firemodeOption, subMenuFiremode)
 
-    local currentFiremodeKey = RAFFunctions.getFiremodeMenuKey(weapon:getFireMode())
+        local currentFiremodeKey = RAFFunctions.getFiremodeMenuKey(weapon:getFireMode())
 
-    for i = 0, weapon:getFireModePossibilities():size() - 1 do
-        local firemode = weapon:getFireModePossibilities():get(i)
-        local firemodeKey = RAFFunctions.getFiremodeMenuKey(firemode)
-        if firemodeKey ~= currentFiremodeKey then
-            subMenuFiremode:addOption(getText("ContextMenu_FireMode_" .. firemode),
-                playerObj, ISInventoryPaneContextMenu.onChangefiremode, weapon, firemode)
+        for i = 0, weapon:getFireModePossibilities():size() - 1 do
+            local firemode = weapon:getFireModePossibilities():get(i)
+            local firemodeKey = RAFFunctions.getFiremodeMenuKey(firemode)
+            if firemodeKey ~= currentFiremodeKey then
+                subMenuFiremode:addOption(getText("ContextMenu_FireMode_" .. firemode),
+                    playerObj, ISInventoryPaneContextMenu.onChangefiremode, weapon, firemode)
+            end
         end
     end
 end
 
 Events.OnServerCommand.Add(RAFFunctions.RAF_OnServerCommand)
+
+if RAFEnabledMods.G93 then
+    function RAFFunctions.cyclicRatePatcher(character, weapon)
+        if not weapon or not character then return end
+        if not weapon:isRanged() then return end
+
+        local aiming = character:isAiming()
+        local mode = weapon:getFireMode()
+
+        if mode == "Auto" or mode == "RealAuto" then
+            if aiming then
+                weapon:setFireMode("RealAuto")
+                weapon:setRecoilDelay(1)
+            elseif (not aiming) then
+                weapon:setFireMode("Auto")
+                weapon:setRecoilDelay(15)
+            end
+        end
+
+        if mode == "Burst" or mode == "RealBurst" then
+            if aiming then
+                weapon:setFireMode("RealBurst")
+                weapon:setRecoilDelay(1)
+            elseif (not aiming) then
+                weapon:setFireMode("Burst")
+                weapon:setRecoilDelay(15)
+            end
+        end
+    end
+
+    function RAFFunctions.weaponUpdater()
+        local character = getPlayer()
+        if not character then return end
+
+        local weapon = character:getPrimaryHandItem()
+        if not weapon or not instanceof(weapon, "HandWeapon") then return end
+        if weapon:getSubCategory() ~= "Firearm" then return end
+
+        RAFFunctions.cyclicRatePatcher(character, weapon)
+    end
+
+    Events.OnPlayerUpdate.Add(RAFFunctions.weaponUpdater)
+end
